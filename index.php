a2c0ef10586a248205a949be0a5b4ab345c41367
Jonathan <colombiano.92@live.com>
2017-07-24 22:06:44 -0500

    minor syntax changes

--- index.php
+++ index.php
@@ -18,7 +18,7 @@
     <link href="../../assets/css/ie10-viewport-bug-workaround.css" rel="stylesheet">
 
     <!-- Custom styles for this template -->
-    <link rel="stylesheet" type="text/css" href="index.css">
+    <link href="index.css" rel="stylesheet" type="text/css">
 
     <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
     <!--[if lt IE 9]>
